34d30bbb4505b2107bc1b4924dbc1f6fc320fbde
Ryan D <p21rdentchev@fairfieldprep.org>
2020-11-17 13:46:22 -0500

    hw 8-1

---
+++ hw8-1.py
@@ -0,0 +1,31 @@
+# Ryan Dentchev 11/17/2020
+
+# Question 1
+my_list = [0, 1, 2, 3, 4, 5, 6, 7]
+
+# Question 2
+print(len(my_list))
+
+# Question 3
+print(my_list[3])
+
+# Question 4
+print(5 not in my_list)
+
+# Question 5
+print(10 not in my_list)
+
+# Question 6
+new_list = ['Alex', 'Azaan', 'Ryan']
+
+# Question 7
+print(new_list[2])
+
+# Question 8
+print(new_list * 3)
+
+# Question 9
+print(my_list + new_list)
+
+# Question 10
+print(len(my_list + new_list))
